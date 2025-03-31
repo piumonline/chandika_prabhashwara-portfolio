cfc3afdf0f5c759975725a9e936a23833c5305da
Piumal Dhanawardhana <24piumal@gmail.com>
2025-03-31 14:34:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,17 @@ import RecentWork from "@/components/RecentWork";
 import TextMaskSection from "@/components/TextMaskSection";
 import WorkExperience from "@/components/WorkExperience";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Your Website Title",
+  description: "Your Website Description",
+  other: {
+    "trustpilot-one-time-domain-verification-id": "056da7dd-a47d-4be3-864c-d2e64163a1ae",
+  },
+};
+
+
 export default function Home() {
   return (
     <main className="">
